655ec622d73bc285b89d64686b7aa650eb444243
Student <student@RU-003.local>
2016-01-28 18:02:45 -0700

    Todo: the move method should be moved to the posts themselves.  Include the validation, etc, there

--- displayController.js
+++ displayController.js
@@ -7,6 +7,9 @@ angular.module('app')
 		$scope.sizeUnit = 'px';
 		$scope.baseHeight = 50;
 		$scope.diskHeight = 50;
+		$scope.baseWidth = 300;
+		$scope.colors = ['#2ecc71','#3498db', '#e74c3c', '#f1c40f', '#d35400' ];
+		$scope.posts = towerData.posts;
 
 		$scope.getDiskWidth = function(diskSize) {
 			return diskSize * $scope.diskWidthCoefficient + $scope.diskWidthMinimum + $scope.sizeUnit;
@@ -14,8 +17,12 @@ angular.module('app')
 		$scope.getDiskHeight = function(index) {
 			return index * $scope.diskHeight + $scope.baseHeight +$scope.sizeUnit;
 		}
-		$scope.posts = towerData.posts
-		console.log($scope.posts)
+		$scope.getDiskPosition = function(diskSize) {
+			return ($scope.baseWidth - (diskSize * $scope.diskWidthCoefficient + $scope.diskWidthMinimum))/ 2 + $scope.sizeUnit;
+		}
+		
+		//console.log($scope.posts[0].contents[]);
+
 	}])
 
 
